9ab68d9892d2e526d7f6d785b7100131a4e46e33
Sebastian <thiagohugo2014@gmail.com>
2023-05-29 11:18:21 -0600

    frontend :)

--- seccion10-Grupo13/src/main/webapp/js/main.js
+++ seccion10-Grupo13/src/main/webapp/js/main.js
@@ -2,14 +2,42 @@ $(document).ready(function(){
     mainSearch();
 });
 
+function runQuery() {
+    const driver = neo4j.driver('bolt://44.203.107.46:7687', neo4j.auth.basic('neo4j', 'calculation-exception-contrasts'));
+    const session = driver.session();
+
+    const query = 'use universities MATCH (n) RETURN (n)';
+
+    session.run(query)
+        .then(result => {
+        const resultDiv = document.getElementById('result');
+        resultDiv.innerHTML = '';
+        result.records.forEach(record => {
+            var node = record.get('n');
+            var prop = node.properties;
+            console.log(prop)
+
+
+        });
+        })
+        .catch(error => {
+        console.error('Error executing Cypher query', error);
+        })
+    .finally(() => {
+        session.close();
+        driver.close();
+    });
+}
+
 function mainSearch(){
     
 };
 
 function searchUniversity(){
+    
 
 }
 
-function filtre(){
+function filter(){
 
 };
